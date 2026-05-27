24c47a4d94d9b2677bc233226f4ba363e67feb9f
johanjavier <20241054@uthh.edu.mx>
2026-05-27 09:09:03 -0600

    Mejoras en compatibilidad DPI y ajustes de interfaz
    Se realizaron los siguientes cambios:
    - En `FrmCompra.Designer.cs`, se cambió el modo de escala de `Font` a `Dpi` y se ajustaron las dimensiones de la escala.
    - En `FrmVentas.Designer.cs`, se ajustaron ubicaciones, tamaños, márgenes y propiedades de varios controles para adaptar la interfaz a una ventana más pequeña.
    - En `FrmVentas`, se cambió el modo de escala de `Font` a `Dpi` y se redujeron las dimensiones de la ventana.
    - En `Program.cs`, se agregó soporte para pantallas de alta densidad de píxeles (DPI) mediante la función `SetProcessDPIAware`.
    
    Estos cambios mejoran la compatibilidad con pantallas de alta densidad y optimizan la interfaz gráfica para resoluciones más compactas.

--- FrmVentas.Designer.cs
+++ FrmVentas.Designer.cs
@@ -52,10 +52,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(1425, 98);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(950, 64);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.Size = new System.Drawing.Size(0, 20);
             this.label3.TabIndex = 101;
             // 
             // pnlcentro
@@ -63,8 +62,9 @@
             this.pnlcentro.Controls.Add(this.dataGridView1);
             this.pnlcentro.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlcentro.Location = new System.Drawing.Point(0, 0);
+            this.pnlcentro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlcentro.Name = "pnlcentro";
-            this.pnlcentro.Size = new System.Drawing.Size(1246, 601);
+            this.pnlcentro.Size = new System.Drawing.Size(831, 391);
             this.pnlcentro.TabIndex = 111;
             // 
             // dataGridView1
@@ -74,10 +74,9 @@
             this.dataGridView1.BackgroundColor = System.Drawing.Color.DarkGray;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(1246, 601);
+            this.dataGridView1.Size = new System.Drawing.Size(831, 391);
             this.dataGridView1.TabIndex = 114;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
@@ -86,9 +85,10 @@
             this.pnlinferior.Controls.Add(this.textBox1);
             this.pnlinferior.Controls.Add(this.btncobrar);
             this.pnlinferior.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlinferior.Location = new System.Drawing.Point(0, 499);
+            this.pnlinferior.Location = new System.Drawing.Point(0, 325);
+            this.pnlinferior.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlinferior.Name = "pnlinferior";
-            this.pnlinferior.Size = new System.Drawing.Size(1246, 102);
+            this.pnlinferior.Size = new System.Drawing.Size(831, 66);
             this.pnlinferior.TabIndex = 112;
             this.pnlinferior.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlinferior_Paint);
             // 
@@ -97,10 +97,9 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.Black;
-            this.textBox1.Location = new System.Drawing.Point(1049, -3);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Location = new System.Drawing.Point(699, -2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(197, 71);
+            this.textBox1.Size = new System.Drawing.Size(133, 50);
             this.textBox1.TabIndex = 119;
             this.textBox1.Text = "$0.00";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
@@ -112,10 +111,9 @@
             this.btncobrar.ForeColor = System.Drawing.Color.Black;
             this.btncobrar.Image = global::PROYECTO_ESTADIA.Properties.Resources.ingreso__1_;
             this.btncobrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btncobrar.Location = new System.Drawing.Point(769, 10);
-            this.btncobrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btncobrar.Location = new System.Drawing.Point(513, 6);
             this.btncobrar.Name = "btncobrar";
-            this.btncobrar.Size = new System.Drawing.Size(272, 45);
+            this.btncobrar.Size = new System.Drawing.Size(181, 29);
             this.btncobrar.TabIndex = 118;
             this.btncobrar.Text = "F12 Cobrar.";
             this.btncobrar.UseVisualStyleBackColor = true;
@@ -130,10 +128,10 @@
             this.pnlsuperior.Controls.Add(this.textBox3);
             this.pnlsuperior.Controls.Add(this.btnbuscar);
             this.pnlsuperior.Controls.Add(this.label7);
-            this.pnlsuperior.Location = new System.Drawing.Point(4, 2);
-            this.pnlsuperior.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlsuperior.Location = new System.Drawing.Point(3, 1);
+            this.pnlsuperior.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.pnlsuperior.Name = "pnlsuperior";
-            this.pnlsuperior.Size = new System.Drawing.Size(1246, 110);
+            this.pnlsuperior.Size = new System.Drawing.Size(828, 72);
             this.pnlsuperior.TabIndex = 114;
             this.pnlsuperior.TabStop = true;
             // 
@@ -144,10 +142,10 @@
             this.btnagregarproducto.ForeColor = System.Drawing.Color.Black;
             this.btnagregarproducto.Image = global::PROYECTO_ESTADIA.Properties.Resources.agregar_p16;
             this.btnagregarproducto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnagregarproducto.Location = new System.Drawing.Point(942, 31);
-            this.btnagregarproducto.Margin = new System.Windows.Forms.Padding(2);
+            this.btnagregarproducto.Location = new System.Drawing.Point(625, 20);
+            this.btnagregarproducto.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.btnagregarproducto.Name = "btnagregarproducto";
-            this.btnagregarproducto.Size = new System.Drawing.Size(180, 31);
+            this.btnagregarproducto.Size = new System.Drawing.Size(120, 20);
             this.btnagregarproducto.TabIndex = 115;
             this.btnagregarproducto.Text = "ENTER - Agr. Prod\r\n";
             this.btnagregarproducto.UseVisualStyleBackColor = true;
@@ -159,11 +157,11 @@
             this.btnReporteventas.ForeColor = System.Drawing.Color.Black;
             this.btnReporteventas.Image = global::PROYECTO_ESTADIA.Properties.Resources.beneficio_financiero;
             this.btnReporteventas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnReporteventas.Location = new System.Drawing.Point(765, 69);
-            this.btnReporteventas.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReporteventas.Location = new System.Drawing.Point(509, 45);
+            this.btnReporteventas.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.btnReporteventas.Name = "btnReporteventas";
-            this.btnReporteventas.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
-            this.btnReporteventas.Size = new System.Drawing.Size(200, 31);
+            this.btnReporteventas.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnReporteventas.Size = new System.Drawing.Size(133, 20);
             this.btnReporteventas.TabIndex = 114;
             this.btnReporteventas.Text = "Reporte de Ventas.";
             this.btnReporteventas.UseVisualStyleBackColor = true;
@@ -175,11 +173,11 @@
             this.btnborrar.ForeColor = System.Drawing.Color.Black;
             this.btnborrar.Image = global::PROYECTO_ESTADIA.Properties.Resources.borrar;
             this.btnborrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnborrar.Location = new System.Drawing.Point(557, 69);
-            this.btnborrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnborrar.Location = new System.Drawing.Point(370, 45);
+            this.btnborrar.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.btnborrar.Name = "btnborrar";
-            this.btnborrar.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
-            this.btnborrar.Size = new System.Drawing.Size(200, 31);
+            this.btnborrar.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnborrar.Size = new System.Drawing.Size(133, 20);
             this.btnborrar.TabIndex = 113;
             this.btnborrar.Text = "DEL Borrar Art.";
             this.btnborrar.UseVisualStyleBackColor = true;
@@ -189,10 +187,9 @@
             this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.ForeColor = System.Drawing.Color.Black;
-            this.textBox3.Location = new System.Drawing.Point(350, 33);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox3.Location = new System.Drawing.Point(232, 21);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(585, 26);
+            this.textBox3.Size = new System.Drawing.Size(391, 20);
             this.textBox3.TabIndex = 111;
             // 
             // btnbuscar
@@ -202,11 +199,11 @@
             this.btnbuscar.ForeColor = System.Drawing.Color.Black;
             this.btnbuscar.Image = global::PROYECTO_ESTADIA.Properties.Resources.busqueda;
             this.btnbuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnbuscar.Location = new System.Drawing.Point(340, 69);
-            this.btnbuscar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnbuscar.Location = new System.Drawing.Point(226, 45);
+            this.btnbuscar.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.btnbuscar.Name = "btnbuscar";
-            this.btnbuscar.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
-            this.btnbuscar.Size = new System.Drawing.Size(200, 31);
+            this.btnbuscar.Padding = new System.Windows.Forms.Padding(2, 0, 0, 0);
+            this.btnbuscar.Size = new System.Drawing.Size(133, 20);
             this.btnbuscar.TabIndex = 112;
             this.btnbuscar.Tag = "  ";
             this.btnbuscar.Text = "   F10 Buscar.";
@@ -218,24 +215,24 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(159, 36);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(105, 23);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(159, 20);
+            this.label7.Size = new System.Drawing.Size(108, 13);
             this.label7.TabIndex = 110;
             this.label7.Text = "Codigo del producto :";
             // 
             // FrmVentas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1246, 601);
+            this.ClientSize = new System.Drawing.Size(831, 391);
             this.Controls.Add(this.pnlsuperior);
             this.Controls.Add(this.pnlinferior);
             this.Controls.Add(this.pnlcentro);
             this.Controls.Add(this.label3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmVentas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmVentas";
--- Program.cs
+++ Program.cs
@@ -8,12 +8,18 @@ namespace PROYECTO_ESTADIA
 {
     internal static class Program
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                SetProcessDPIAware();
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmPrincipal());
